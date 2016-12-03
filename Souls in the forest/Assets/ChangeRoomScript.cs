8eba85b57baa5966bf586dd41e4d59349b1a3fe8
DESKTOP-JP1ALGG\Axel <axelburman123@gmail.com>
2016-12-03 16:47:17 +0100

    Test sener

---
+++ Souls in the forest/Assets/ChangeRoomScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeRoomScript : MonoBehaviour 
+{
+	public string m_NextRoom = "";
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+		if (coll.gameObject.tag == "Player") 
+		{
+			DontDestroyOnLoad(coll.gameObject);
+			Application.LoadLevel (m_NextRoom);
+		}
+
+	}
+}
---
+++ Souls in the forest/Assets/GameState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	public static GameState Singleton = null;
+	// Use this for initialization
+	void Start () 
+	{
+		Singleton = this;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	GameObject GetPlayer()
+	{
+		return gameObject;
+	}
+}
