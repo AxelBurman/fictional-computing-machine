2b66b1743754b3e77ebfa2a59ae15c5f53d1c7af
DESKTOP-JP1ALGG\Axel <axelburman123@gmail.com>
2016-12-03 17:45:08 +0100

    Camera offset added

--- Souls in the forest/Assets/GameState.cs
+++ Souls in the forest/Assets/GameState.cs
@@ -5,6 +5,7 @@ public class GameState : MonoBehaviour {
 
 	public static GameState Singleton = null;
     public Transform m_Camera;
+    public float m_CameraOffsettY = 0;
 	// Use this for initialization
 	void Start () 
 	{
@@ -15,7 +16,7 @@ public class GameState : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-        m_Camera.position =  new Vector3(transform.position.x, transform.position.y, -10.0f);
+        m_Camera.position =  new Vector3(transform.position.x, transform.position.y + m_CameraOffsettY, -10.0f);
 
     }
 
