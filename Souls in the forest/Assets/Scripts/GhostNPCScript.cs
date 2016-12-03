3b51516558c9d6879a7e8df8c28c043525e3eb3e
DESKTOP-JP1ALGG\Axel <axelburman123@gmail.com>
2016-12-03 23:04:15 +0100

    Ghosts and climbing

---
+++ Souls in the forest/Assets/Scripts/GhostNPCScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public class GhostNPCScript : MonoBehaviour
+{
+    public List<string> m_dialogueOnInteract = new List<string>();
+    protected int m_CurrentMilestone = 0;
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void NextMilestone()
+    {
+        m_CurrentMilestone++;
+    }
+
+    void Interact()
+    {
+        Debug.Log(m_dialogueOnInteract[m_CurrentMilestone]);
+    }
+}
--- Souls in the forest/Assets/Scripts/PlayerActions.cs
+++ Souls in the forest/Assets/Scripts/PlayerActions.cs
@@ -4,37 +4,70 @@ using System.Collections;
 public class PlayerActions : MonoBehaviour
 {
 
+    public float m_ClimbSpeed = 0.5f;
+
     private Transform m_CurrentInteractCollider;
 
-    private bool m_IsInsideInteractCollider;
+    private bool m_IsInsideInteractCollider = false;
+    private bool m_CanClimb = false;
 
-	// Use this for initialization
-	void Start ()
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start ()
     {
-	    
-	}
+        rb = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    if(Input.GetKeyDown(KeyCode.I))
+        if (m_IsInsideInteractCollider)
         {
-            if(m_IsInsideInteractCollider)
+            if (Input.GetKeyDown(KeyCode.I))
             {
                 m_CurrentInteractCollider.SendMessage("OnInteract");
+            }
+        }
+
+        if (m_CanClimb)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.MovePosition(rb.position + Vector2.up * m_ClimbSpeed);
+                
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                rb.MovePosition(rb.position + Vector2.down * m_ClimbSpeed);
             }
         }
 	}
 
     void EnterInteract(Transform i_Trans)
     {
-        m_CurrentInteractCollider = i_Trans;
-        m_IsInsideInteractCollider = true;
+        if (m_CurrentInteractCollider != i_Trans)
+        {
+            m_CurrentInteractCollider = i_Trans;
+            m_IsInsideInteractCollider = true;
+        }
     }
 
     void ExitInteract(Transform i_Trans)
     {
         m_CurrentInteractCollider = null;
         m_IsInsideInteractCollider = false;
+    }
+
+    void EnterClimb(Transform i_Trans)
+    {
+        m_CanClimb = true;
+        rb.gravityScale = 0;
+    }
+
+    void ExitClimb(Transform i_Trans)
+    {
+        m_CanClimb = false;
+        rb.gravityScale = 3;
     }
 }
