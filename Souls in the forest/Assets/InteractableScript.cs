34a29e98ff43a66f66b4c28f20f9248cb2987a00
DESKTOP-JP1ALGG\Axel <axelburman123@gmail.com>
2016-12-03 19:23:45 +0100

    Interact collider prefab

--- Souls in the forest/Assets/InteractableScript.cs
+++ Souls in the forest/Assets/InteractableScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class InteractableScript : MonoBehaviour {
 
     public string m_MessageOnInteract = "";
+    public Transform m_TargetObject;
 	// Use this for initialization
 	void Start ()
     {
@@ -16,8 +17,18 @@ public class InteractableScript : MonoBehaviour {
 	    
 	}
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.SendMessage("EnterInteract", transform);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        other.transform.SendMessage("ExitInteract", transform);
+    }
+
     void OnInteract()
     {
-
+        m_TargetObject.SendMessage(m_MessageOnInteract);
     }
 }
---
+++ Souls in the forest/Assets/PlayerActions.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerActions : MonoBehaviour
+{
+
+    private Transform m_CurrentInteractCollider;
+
+    private bool m_IsInsideInteractCollider;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.I))
+        {
+            if(m_IsInsideInteractCollider)
+            {
+                m_CurrentInteractCollider.SendMessage("OnInteract");
+            }
+        }
+	}
+
+    void EnterInteract(Transform i_Trans)
+    {
+        m_CurrentInteractCollider = i_Trans;
+        m_IsInsideInteractCollider = true;
+    }
+
+    void ExitInteract(Transform i_Trans)
+    {
+        m_CurrentInteractCollider = null;
+        m_IsInsideInteractCollider = false;
+    }
+}
